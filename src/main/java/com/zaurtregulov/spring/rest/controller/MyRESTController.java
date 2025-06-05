4099a08f43ec23c6f83e1bf1a3c7d8e9382981f5
kir <kirpro95@yandex.ru>
2025-06-05 11:59:23 +0300

    обработка исключений

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRESTController.java
@@ -1,13 +1,10 @@
 package com.zaurtregulov.spring.rest.controller;
 
 import com.zaurtregulov.spring.rest.entity.Employee;
+import com.zaurtregulov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -25,7 +22,13 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                    id + "in database");
+        }
         return employee;
     }
+
+
 
 }
